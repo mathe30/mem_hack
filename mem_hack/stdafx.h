5919ea2aca57415080a6ceb2b6d6d2aab0b04d02
matheo <matheo@FedoraBox.mynet>
2019-03-10 04:03:03 -0400

    binary file has been removed , stdafx modified after the merge commit

--- mem_hack/stdafx.h
+++ mem_hack/stdafx.h
@@ -1,13 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-<<<<<<< HEAD
 #include "tools/tools.h"
 #include "routines/routines.h"
 #include "hack/hack.h"
 #include "cloop/cloop.h"
-=======
-#include "tools.h"
-#include "routines.h"
-#include "hack.h"
-#include "cloop.h"
->>>>>>> ba0276a187caa9f3643b02b94461cf7ad7e44e30
